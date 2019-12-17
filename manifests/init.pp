91714d67d1c02238479f44e0368f72eab541cd3b
Jakov Sosic <jsosic@neogov.net>
2019-12-17 13:58:43 +0100

    Add params to decide whether to manage service

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,17 +3,23 @@
 #
 # This module deploys lldpd
 #
-class lldpd {
+class lldpd (
+  $service_ensure = 'running',
+  $service_enable = true,
+){
 
   if "x${::is_virtual}" == 'xfalse' {
+
     package { 'lldpd':
       ensure => present,
     }
+
     service { 'lldpd':
-      ensure   => running,
-      enable   => true,
-      require  => Package['lldpd'],
+      ensure  => $service_ensure,
+      enable  => $service_enable,
+      require => Package['lldpd'],
     }
+
   }
 
 }
